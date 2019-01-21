629034a89ef2bf9f2cd8aff39309a8adca1053b2
Barsukovskii Mikhail <barsukovsky.mo@gmail.com>
2019-01-21 16:12:09 +0500

    LWDEV-10347 settings from json

--- src/main/kotlin/com/lykke/me/test/client/spring/config/ApplicationConfig.kt
+++ src/main/kotlin/com/lykke/me/test/client/spring/config/ApplicationConfig.kt
@@ -1,26 +1,32 @@
 package com.lykke.me.test.client.spring.config
 
 import com.lykke.me.test.client.config.Config
-import com.lykke.me.test.client.config.IpEndpointConfig
-import com.lykke.me.test.client.config.MeTestClientConfig
-import com.lykke.me.test.client.config.RabbitMqConfig
-import org.springframework.beans.factory.annotation.Value
+import com.lykke.utils.config.ConfigInitializer
+import org.apache.log4j.Logger
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+import org.springframework.core.env.Environment
 
 @Configuration
 open class ApplicationConfig {
 
-    @Bean
-    open fun config(@Value("\${matching.engine.host}")
-                    host: String,
-                    @Value("\${matching.engine.port}")
-                    port: Int): Config {
-        // todo read from json
-        return Config(MeTestClientConfig(IpEndpointConfig(host, port),
-                setOf(RabbitMqConfig("amqp://lykke.history:lykke.history@127.0.0.1:5672",
-                        "lykke.spot.matching.engine.out.events",
-                        "lykke.spot.matching.engine.out.events.test.client.cashIn",
-                        "1"))))
+    companion object {
+        private val LOGGER = Logger.getLogger(ApplicationConfig::class.java.name)
     }
+
+    @Bean
+    open fun config(environment: Environment): Config {
+        return if (environment.acceptsProfiles("local_config")) {
+            ConfigInitializer.initConfig("local", classOfT = Config::class.java)
+        } else {
+            val commandLineArgs = environment.getProperty("nonOptionArgs", Array<String>::class.java)
+            if (commandLineArgs == null) {
+                val errorMessage = "Not enough args. Usage: httpConfigString"
+                LOGGER.error(errorMessage)
+                throw IllegalArgumentException(errorMessage)
+            }
+            ConfigInitializer.initConfig(commandLineArgs[0], classOfT = Config::class.java)
+        }
+    }
+
 }
