ac38dd13ec4c67921818958ef582a7c05a00ad96
papchenko <rocketmannot@gmail.com>
2019-01-21 15:28:16 +0200

    LWDEV-10346 add banner add endpoint to get list of all tests, return session id on tests start

--- src/main/kotlin/com/lykke/me/test/client/service/TestsService.kt
+++ src/main/kotlin/com/lykke/me/test/client/service/TestsService.kt
@@ -3,9 +3,9 @@ package com.lykke.me.test.client.service
 import com.lykke.me.test.client.web.dto.TestSessionsDto
 
 interface TestsService {
-    fun startAllTests(runTestsPolicy: RunTestsPolicy = RunTestsPolicy.CONTINUE_ON_ERROR)
+    fun startAllTests(runTestsPolicy: RunTestsPolicy = RunTestsPolicy.CONTINUE_ON_ERROR): String?
 
-    fun startTests(testNames: Set<String>, runTestsPolicy: RunTestsPolicy = RunTestsPolicy.CONTINUE_ON_ERROR)
+    fun startTests(testNames: Set<String>, runTestsPolicy: RunTestsPolicy = RunTestsPolicy.CONTINUE_ON_ERROR): String?
 
     fun getTestNames(): Set<String>
 
