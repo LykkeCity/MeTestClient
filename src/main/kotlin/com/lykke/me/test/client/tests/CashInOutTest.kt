c899055a888d9046728f115d0705eab875de7b24
papchenko <rocketmannot@gmail.com>
2019-01-22 16:22:08 +0200

    LWDEV-10361 implement cashtransfer test

--- src/main/kotlin/com/lykke/me/test/client/tests/CashInOutTest.kt
+++ src/main/kotlin/com/lykke/me/test/client/tests/CashInOutTest.kt
@@ -6,6 +6,7 @@ import com.lykke.me.test.client.outgoing.messages.common.FeeSizeType
 import com.lykke.me.test.client.outgoing.messages.common.FeeType
 import com.lykke.me.test.client.outgoing.messages.common.SimpleFeeInstruction
 import com.lykke.me.test.client.outgoing.messages.utils.MessageBuilder
+import com.lykke.me.test.client.utils.generateMessages
 import org.springframework.beans.factory.annotation.Autowired
 import java.math.BigDecimal
 
@@ -23,43 +24,40 @@ class CashInOutTest {
 
     fun cashInTest() {
         val result = ArrayList<Message>(TEST_RUN_COUNT * 2)
-        result.addAll(generateCashInMessages(TEST_RUN_COUNT, CLIENT1, "BTC", BigDecimal.valueOf(0.1), listOf(SimpleFeeInstruction(FeeType.PERCENTAGE,
-                FeeSizeType.CLIENT_FEE, BigDecimal.valueOf(0.01), CLIENT1, CLIENT3, listOf("BTC")))))
-        result.addAll(generateCashInMessages(TEST_RUN_COUNT, CLIENT2, "USD", BigDecimal.valueOf(0.1), listOf(SimpleFeeInstruction(FeeType.PERCENTAGE,
-                FeeSizeType.CLIENT_FEE, BigDecimal.valueOf(0.01), CLIENT1, CLIENT3, listOf("USD")))))
+        result.addAll(generateMessages(TEST_RUN_COUNT, getStrategy(CLIENT1, "BTC", BigDecimal.valueOf(0.1), listOf(SimpleFeeInstruction(FeeType.PERCENTAGE,
+                FeeSizeType.CLIENT_FEE, BigDecimal.valueOf(0.01), CLIENT1, CLIENT3, listOf("BTC"))))))
+
+        result.addAll(generateMessages(TEST_RUN_COUNT, getStrategy(CLIENT2, "USD", BigDecimal.valueOf(0.1), listOf(SimpleFeeInstruction(FeeType.PERCENTAGE,
+                FeeSizeType.CLIENT_FEE, BigDecimal.valueOf(0.01), CLIENT1, CLIENT3, listOf("USD"))))))
 
         result.forEach(meClient::sendMessage)
     }
 
     fun cashOutTest() {
         val result = ArrayList<Message>(TEST_RUN_COUNT * 2)
-        result.addAll(generateCashInMessages(TEST_RUN_COUNT, CLIENT1, "BTC", BigDecimal.valueOf(-0.1)))
-        result.addAll(generateCashInMessages(TEST_RUN_COUNT, CLIENT2, "USD", BigDecimal.valueOf(-0.1)))
+        result.addAll(generateMessages(TEST_RUN_COUNT, getStrategy(CLIENT1, "BTC", BigDecimal.valueOf(-0.1))))
+        result.addAll(generateMessages(TEST_RUN_COUNT, getStrategy(CLIENT2, "USD", BigDecimal.valueOf(-0.1))))
 
         result.forEach(meClient::sendMessage)
     }
 
     fun cashInInputValidationFailed() {
-        generateCashInMessages(TEST_RUN_COUNT, CLIENT1, "BTC", BigDecimal.valueOf(0.000000001), listOf(SimpleFeeInstruction(FeeType.PERCENTAGE,
-                FeeSizeType.CLIENT_FEE, BigDecimal.valueOf(0.01), CLIENT1, CLIENT3, listOf("BTC")))).forEach(meClient::sendMessage)
+        generateMessages(TEST_RUN_COUNT, getStrategy(CLIENT1, "BTC", BigDecimal.valueOf(0.000000001), listOf(SimpleFeeInstruction(FeeType.PERCENTAGE,
+                FeeSizeType.CLIENT_FEE, BigDecimal.valueOf(0.01), CLIENT1, CLIENT3, listOf("BTC"))))).forEach(meClient::sendMessage)
     }
 
 
     fun cashInBusinessValidationFailed() {
-        generateCashInMessages(TEST_RUN_COUNT, CLIENT1, "BTC", BigDecimal.valueOf(-0.1), listOf(SimpleFeeInstruction(FeeType.PERCENTAGE,
-                FeeSizeType.CLIENT_FEE, BigDecimal.valueOf(0.01), CLIENT1, CLIENT3, listOf("BTC")))).forEach(meClient::sendMessage)
+        generateMessages(TEST_RUN_COUNT, getStrategy(CLIENT1, "BTC", BigDecimal.valueOf(-0.1), listOf(SimpleFeeInstruction(FeeType.PERCENTAGE,
+                FeeSizeType.CLIENT_FEE, BigDecimal.valueOf(0.01), CLIENT1, CLIENT3, listOf("BTC"))))).forEach(meClient::sendMessage)
     }
 
 
-    private fun generateCashInMessages(count: Int,
-                                       clientId: String,
-                                       assetId: String,
-                                       volume: BigDecimal,
-                                       fees: List<SimpleFeeInstruction> = emptyList()): List<Message> {
-        val listOfMessages = ArrayList<Message>()
-        for (i in IntRange(0, count)) {
-            listOfMessages.add(messageBuilder.buildCashInOutMessage(clientId, assetId, volume, fees))
-        }
-        return listOfMessages
+    private fun getStrategy(
+            clientId: String,
+            assetId: String,
+            volume: BigDecimal,
+            fees: List<SimpleFeeInstruction> = emptyList()): () -> Message {
+        return { messageBuilder.buildCashInOutMessage(clientId, assetId, volume, fees) }
     }
 }
