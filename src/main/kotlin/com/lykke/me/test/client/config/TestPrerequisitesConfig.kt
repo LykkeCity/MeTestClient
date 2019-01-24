1781513af079052b030740d5505c5850ae004cef
papchenko <rocketmannot@gmail.com>
2019-01-24 11:45:20 +0200

    LWDEV-10346 add config for assets and client

--- src/main/kotlin/com/lykke/me/test/client/config/TestPrerequisitesConfig.kt
+++ src/main/kotlin/com/lykke/me/test/client/config/TestPrerequisitesConfig.kt
@@ -1,7 +1,8 @@
 package com.lykke.me.test.client.config
 
 data class TestPrerequisitesConfig(val clientsConfig: ClientsConfig,
-                                   val assetsConfig: Set<AssetPairConfig>) {
+                                   val assetsConfig: Set<AssetPairConfig>,
+                                   val maxOrdersInOrderBook: Int) {
     class ClientsConfig(val trustedClients: Set<String>, clients: Set<String>)
     class AssetPairConfig(val assetPairId: String, val baseAssetId: String, val quotingAssetId: String)
 }
